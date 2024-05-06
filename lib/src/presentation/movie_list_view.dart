e4748c81f8f9622683e814fac16cb0642f631ebc
Gabriel Mattano <gabrielbarbosam@gmail.com>
2024-05-06 13:23:27 -0300

    Adiciona estrutura de BottomBar, adiciona logo em AppBar e multiplica lista de filmes por categoria.

--- lib/src/presentation/movie_list_view.dart
+++ lib/src/presentation/movie_list_view.dart
@@ -1,4 +1,7 @@
+// ignore_for_file: avoid_unnecessary_containers, prefer_const_constructors
+
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_filmes/src/settings/settings_controller.dart';
 import 'package:flutter_filmes/src/settings/settings_view.dart';
 
@@ -12,60 +15,147 @@ class MovieListView extends StatelessWidget {
   final List<MovieItem> items = repository.moviesList();
 
   MovieListView({
-    super.key,
+    Key? key,
     required this.settingsController,
-  });
+  }) : super(key: key);
+
+  AssetImage checkThemeMode(BuildContext context) {
+    final Brightness currentBrightness = Theme.of(context).brightness;
+    return currentBrightness == Brightness.dark
+        ? const AssetImage('assets/images/logo_white.png')
+        : const AssetImage('assets/images/logo_black.png');
+  }
 
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       appBar: AppBar(
-        title: const Text('FILMES'),
-        actions: [
+        centerTitle: true,
+        title: SizedBox(
+          height: AppBar().preferredSize.height*0.7,
+          child: Image(
+            image: checkThemeMode(context),),
+        ),
+
+          actions: [
           IconButton(
-              icon: const Icon(Icons.settings),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => SettingsView(controller: settingsController),
-                  ),
-                );
-              }
-          ),
-        ],
-      ),
-      body: ListView.builder(
-        scrollDirection: Axis.horizontal,
-        itemCount: items.length,
-        itemBuilder: (BuildContext context, int index) {
-          final item = items[index];
-          return GestureDetector(
-            onTap: () {
+            icon: const Icon(Icons.settings),
+            onPressed: () {
               Navigator.push(
                 context,
                 MaterialPageRoute(
-                  builder: (context) => MovieDetailsView(movie: item),
+                  builder: (context) => SettingsView(controller: settingsController),
                 ),
               );
             },
-            child: Container(
-              margin: const EdgeInsets.all(8.0),
-              child: Column(
-                children: [
-                  const SizedBox(height: 8),
-                  Image.asset(
-                      item.posterPath,
-                      width: 200,
-                      height: 250,),
-                  Text(item.title, style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-                ],
-              ),
-            ),
-          );
-        },
+          ),
+        ],
+      ),
+      body: _buildBody(),
+      bottomNavigationBar: _buildBottomNavigationBar(),
+    );
+  }
+
+  Widget _buildBody() {
+    return SafeArea(
+      child: ListView(
+        children: [
+          _buildCategoryListView('Ação', items),
+          _buildCategoryListView('Comédia', items),
+          _buildCategoryListView('Drama', items),
+          _buildCategoryListView('Animação', items),
+          // Adicione mais categorias conforme necessário
+        ],
+      ),
+    );
+  }
+
+  Widget _buildCategoryListView(String category, List<MovieItem> items) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+          child: Text(category, style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
+        ),
+        Container(
+          height: 250,
+          child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemCount: items.length,
+            itemBuilder: (BuildContext context, int index) {
+              final item = items[index];
+              return GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => MovieDetailsView(movie: item),
+                    ),
+                  );
+                },
+                child: Container(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            ClipRRect(
+                              borderRadius: BorderRadius.circular(20.0),
+                              child: Image.asset(
+                                item.posterPath,
+                                fit: BoxFit.cover,
+                                height: 200,
+                              ),
+                            ),
+                            SizedBox(height: 8),
+                            Text(
+                              item.title,
+                              style: TextStyle(fontSize: 16, fontWeight: FontWeight.normal),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            },
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildBottomNavigationBar() {
+    return BottomAppBar(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          IconButton(
+            icon: Icon(Icons.home),
+            onPressed: () {
+              // Implemente a ação para a tela inicial
+            },
+          ),
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {
+              // Implemente a ação para a tela de pesquisa
+            },
+          ),
+          IconButton(
+            icon: Icon(Icons.favorite),
+            onPressed: () {
+              // Implemente a ação para a tela de perfil
+            },
+          ),
+        ],
       ),
     );
   }
 }
-
