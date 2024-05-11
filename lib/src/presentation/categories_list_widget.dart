7ccdcadb73fb6aa1abbb46add09ab6b2c5634641
guifx <guifxb@yahoo.com>
2024-05-11 17:57:10 -0300

    chore: separa dados de repository chore: separa widgets de bottom app bar e movie list ao invés de usar helper, como o Lucas sugeriu

---
+++ lib/src/presentation/categories_list_widget.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_filmes/src/data/movie_item.dart';
+
+import 'movie_details_view.dart';
+
+class CategoriesListWidget extends StatelessWidget {
+  final List<List<MovieItem>> categories;
+  final List<String> categoryNames;
+
+  const CategoriesListWidget(
+      {required this.categories, required this.categoryNames, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    {
+      return ListView.builder(
+        itemCount: categories.length,
+        itemBuilder: (context, index) {
+          return Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+                child: Text(
+                  categoryNames[index],
+                  style: const TextStyle(
+                      fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+              ),
+              SizedBox(
+                height: 340,
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: categories[index].length,
+                  itemBuilder: (context, innerIndex) {
+                    final item = categories[index][innerIndex];
+                    final heroTag = '$index-${item.posterPath}';
+
+                    return GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => MovieDetailsView(
+                              movie: item,
+                              heroTag: heroTag,
+                            ),
+                          ),
+                        );
+                      },
+                      child: Container(
+                        margin: const EdgeInsets.symmetric(horizontal: 8.0),
+                        width: 160,
+                        child: Column(
+                          children: [
+                            Hero(
+                              tag: heroTag,
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(6.0),
+                                child: Image.asset(
+                                  item.posterPath,
+                                  fit: BoxFit.fill,
+                                  height: 250,
+                                ),
+                              ),
+                            ),
+                            const SizedBox(height: 12.0),
+                            Text(
+                              item.title,
+                              style: const TextStyle(fontSize: 16),
+                              maxLines: 3,
+                              overflow: TextOverflow.ellipsis,
+                              textAlign: TextAlign.center,
+                            ),
+                          ],
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ],
+          );
+        },
+      );
+    }
+  }
+}
